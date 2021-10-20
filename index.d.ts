0dd472c6b861d6380ec9b4a58b5b60049bf5c65e
Simone Corsi <simonecorsi.rm@gmail.com>
2021-10-20 10:26:31 +0200

    feat: adds ts types

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export = isAsyncFunction;
+/**
+ * Checks if input parameter is an AsyncFunction
+ * @param {(...params) => any | any} fn
+ * @returns boolean
+ */
+declare function isAsyncFunction(fn: (...params: any[]) => any | any): boolean;
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-module.exports = (fn) => {
+/**
+ * Checks if input parameter is an AsyncFunction
+ * @param {(...params) => any | any} fn
+ * @returns boolean
+ */
+function isAsyncFunction(fn) {
   if (typeof fn !== 'function') return false;
   return fn[Symbol.toStringTag] === 'AsyncFunction';
-};
+}
+
+module.exports = isAsyncFunction;
